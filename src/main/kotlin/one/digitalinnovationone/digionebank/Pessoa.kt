e19e0ca4ef5c9e2cbc0bc237e918973a238db241
Bruno Leonardo <brunoleorosa@gmail.com>
2021-07-04 20:00:52 -0300

    Criação da classe Pessoa

---
+++ src/main/kotlin/one/digitalinnovationone/digionebank/Pessoa.kt
@@ -0,0 +1,12 @@
+package one.digitalinnovationone.digionebank
+
+class Pessoa {
+    var nome:String = "Bruno"
+    var cpf:String = "123.123.123-12"
+}
+
+fun main(){
+    val bruno = Pessoa()
+    println(bruno.nome)
+    println(bruno.cpf)
+}
