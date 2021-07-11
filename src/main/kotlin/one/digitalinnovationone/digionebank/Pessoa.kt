199515b6197bccbe0052bef45db414f10dba6da8
Bruno Leonardo <brunoleorosa@gmail.com>
2021-07-11 17:33:13 -0300

    criação da classe Conta

--- src/main/kotlin/one/digitalinnovationone/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovationone/digionebank/Pessoa.kt
@@ -4,11 +4,14 @@ class Pessoa {
     var nome:String = "Bruno"
     var cpf:String = "123.123.123-12"
     private set
+
+    constructor()
+
+    fun infoPessoa() = "Nome: $nome - CPF: $cpf"
 }
 
 fun main(){
     val bruno = Pessoa()
 
-    println(bruno.nome)
-    println(bruno.cpf)
+    println(bruno.infoPessoa())
 }
