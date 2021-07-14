dda68879033e4bf3adf8f022d9971637dfd6f857
Bruno Leonardo <brunoleorosa@gmail.com>
2021-07-14 10:42:58 -0300

    criação das Enum Class ClienteTipo

---
+++ src/main/kotlin/one/digitalinnovationone/digionebank/ClienteTipo.kt
@@ -0,0 +1,6 @@
+package one.digitalinnovationone.digionebank
+
+enum class ClienteTipo(val descricao: String) {
+    PF("Pessoa Física"),
+    PJ("Pessoa Jurídica");
+}
---
+++ src/main/kotlin/one/digitalinnovationone/digionebank/testes/TesteClienteTipoEnum.kt
@@ -0,0 +1,15 @@
+package one.digitalinnovationone.digionebank.testes
+
+import one.digitalinnovationone.digionebank.ClienteTipo
+
+fun main(){
+    ClienteTipo.values().forEach {
+        println("${it.name} - ${it.descricao}")
+    }
+
+    val pf = ClienteTipo.PF
+    println("${pf.name} - ${pf.descricao}")
+
+    val pj = ClienteTipo.PJ
+    println("${pj.name} - ${pj.descricao}")
+}
